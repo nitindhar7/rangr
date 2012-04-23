dad4a16d54aa69c09777b952f9c2741a451bc4b2
Nitin Dhar <nitindhar7@yahoo.com>
2012-04-23 00:54:17 -0300

    adding GA tracking for base controller

--- src/js/controller/base_controller.js
+++ src/js/controller/base_controller.js
@@ -1,3 +1,15 @@
+/* GA TRACKING START */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-17328502-6']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+/* GA TRACKING END */
+
 chrome.extension.onRequest.addListener(requestHandler);
 
 var NOTIFICATION_TIMEOUT_MILLIS = 1000 * 60 * 15;
